cf09b8667fc0b601340dc71454b9cad50c466962
Torsten Scholz <doaschdn@googlemail.com>
2017-07-28 21:21:23 +0200

    Add materialize design to polls page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,20 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require_tree .
+//= require jquery
+//= require turbolinks
+//= require materialize-sprockets
+
+$(document).ready(function() {
+    $('select').material_select();
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+});
