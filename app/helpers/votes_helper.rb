20396ea758d02e587e67bf646e1fc6400b5c98be
Torsten Scholz <doaschdn@googlemail.com>
2017-07-29 18:14:02 +0200

    Display poll and item name for votes listing

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,9 @@
 module VotesHelper
+  def get_poll_for_proposal(proposal)
+    Poll.find(proposal.poll_id)
+  end
+
+  def get_item_for_proposal(proposal)
+    Item.find(proposal.item_id)
+  end
 end
